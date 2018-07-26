3f2d376aed4cdc4196a29e6b3aa8d7b0a288d947
Shashi .K <6208101+shashikamale@users.noreply.github.com>
2018-07-26 09:48:55 +0530

    test case cleanup

--- DAY3/MyAppShashiDay3B/src/app/high-light.directive.spec.ts
+++ DAY3/MyAppShashiDay3B/src/app/high-light.directive.spec.ts
@@ -1,8 +1,8 @@
 import { HighLightDirective } from './high-light.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('HighLightDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighLightDirective();
-    expect(directive).toBeTruthy();
+
   });
 });
